59bf09ddabaa8892ea800d2d692374438491db84
BeijerELCdocs <BeijerELCdocs@users.noreply.github.com>
2023-02-23 08:51:18 +0000

    Changes committed automatically using GitHub Actions.

--- 1132-Help_Center-html5/out/js/mapping/en-mapping.js
+++ 1132-Help_Center-html5/out/js/mapping/en-mapping.js
@@ -5,7 +5,9 @@ var mapping =
             {"appid":"TEST2", "helptopic":"10762-working-with-projects.html"},
 {"appid":"TEST", "helptopic":"10762-working-with-projects.html#UUID-22ac8b69-7fae-9dde-bc25-3441424756e9"},
 {"appid":"TEST2", "helptopic":"34907-working-with-projects.html"},
-{"appid":"TEST", "helptopic":"34907-working-with-projects.html#UUID-881b0937-ca8c-d2b3-b43a-876cb2160f06"}
+{"appid":"TEST", "helptopic":"34907-working-with-projects.html#UUID-881b0937-ca8c-d2b3-b43a-876cb2160f06"},
+{"appid":"TEST2", "helptopic":"36855-working-with-projects.html"},
+{"appid":"TEST", "helptopic":"36855-working-with-projects.html#UUID-c62cbf60-74c6-26bf-565d-c9a496e2f865"}
             
 
          ]}
